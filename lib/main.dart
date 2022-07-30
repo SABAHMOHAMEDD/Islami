797a43766b28cfe9c176356e01be171e053cac16
SABAHMOHAMEDD <102183714+SABAHMOHAMEDD@users.noreply.github.com>
2022-07-30 02:01:06 +0200

    Intialized project added empty screen

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:islami/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: HomeScreen.RouteName ,
+      routes:{
+        HomeScreen.RouteName :(_)=>HomeScreen(),
+
+      } ,
+
+
+    );
+
+
+  }}
+
+
+
